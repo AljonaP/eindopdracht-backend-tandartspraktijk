8129031783a621f99e88bba6d218ecacbaaec3ed
AljonaP <Bartosh_78@hotmail.com>
2022-10-19 12:49:12 +0200

    TreatmentService has been created

---
+++ src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/TreatmentService.java
@@ -0,0 +1,13 @@
+package nl.haaientanden.eindopdrachtbackendtandartspraktijk.services;
+
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.repositories.TreatmentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TreatmentService {
+    private final TreatmentRepository treatmentRepository;
+
+    public TreatmentService(TreatmentRepository treatmentRepository) {
+        this.treatmentRepository = treatmentRepository;
+    }
+}
