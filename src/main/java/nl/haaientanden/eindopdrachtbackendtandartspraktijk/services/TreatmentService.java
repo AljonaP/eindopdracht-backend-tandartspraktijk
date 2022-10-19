dab0ea31660cb8e05ef6980d73f3b95d1ba7f5d2
AljonaP <Bartosh_78@hotmail.com>
2022-10-19 13:30:32 +0200

    TreatmentService: fromDtoToTreatment() and fromTreatmentToDto() have been added

--- src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/TreatmentService.java
+++ src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/TreatmentService.java
@@ -1,5 +1,8 @@
 package nl.haaientanden.eindopdrachtbackendtandartspraktijk.services;
 
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.TreatmentDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.TreatmentInputDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.models.Treatment;
 import nl.haaientanden.eindopdrachtbackendtandartspraktijk.repositories.TreatmentRepository;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +10,27 @@ import org.springframework.stereotype.Service;
 public class TreatmentService {
     private final TreatmentRepository treatmentRepository;
 
-
     public TreatmentService(TreatmentRepository treatmentRepository) {
         this.treatmentRepository = treatmentRepository;
+    }
+
+    public static Treatment fromDtoToTreatment(TreatmentInputDto dto) {
+        var treatment = new Treatment();
+
+        treatment.setTreatmentCode(dto.getTreatmentCode());
+        treatment.setTreatmentfDescription(dto.getTreatmentfDescription());
+        treatment.setTreatmentRate(dto.getTreatmentRate());
+
+        return treatment;
+    }
+
+    public static TreatmentDto fromTreatmentToDto(Treatment treatment) {
+        TreatmentDto dto = new TreatmentDto();
+
+        dto.setTreatmentCode(treatment.getTreatmentCode());
+        dto.setTreatmentfDescription(treatment.getTreatmentfDescription());
+        dto.setTreatmentRate(treatment.getTreatmentRate());
+
+        return dto;
     }
 }
