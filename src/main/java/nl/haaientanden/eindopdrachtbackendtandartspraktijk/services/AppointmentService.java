06bbe324389becc3373214937a1f9d4449d17add
AljonaP <Bartosh_78@hotmail.com>
2022-10-21 18:32:08 +0200

    AppointmentService: fromDtoToAppointment() and fromAppointmentToDto() have been added

--- src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/AppointmentService.java
+++ src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/AppointmentService.java
@@ -1,5 +1,11 @@
 package nl.haaientanden.eindopdrachtbackendtandartspraktijk.services;
 
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.AppointmentDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.AppointmentInputDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.TreatmentDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.TreatmentInputDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.models.Appointment;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.models.Treatment;
 import nl.haaientanden.eindopdrachtbackendtandartspraktijk.repositories.AppointmentRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +17,29 @@ public class AppointmentService {
     public AppointmentService(AppointmentRepository appointmentRepository) {
         this.appointmentRepository = appointmentRepository;
     }
+
+    public static Appointment transferToAppointment(AppointmentInputDto dto) {
+
+        var appointment = new Appointment();
+
+        appointment.setNameDentist(dto.getNameDentist());
+        appointment.setSurnameDentist(dto.getSurnameDentist());
+        appointment.setAppointmentDate(dto.getAppointmentDate());
+        appointment.setAppointmentTime(dto.getAppointmentTime());
+
+        return appointment;
+    }
+
+    public static AppointmentDto transferToDto(Appointment appointment) {
+
+        AppointmentDto dto = new AppointmentDto();
+
+        dto.setNameDentist(appointment.getNameDentist());
+        dto.setSurnameDentist(appointment.getSurnameDentist());
+        dto.setAppointmentDate(appointment.getAppointmentDate());
+        dto.setAppointmentTime(appointment.getAppointmentTime());
+
+        return dto;
+    }
+
 }
