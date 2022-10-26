79ce51a086897fd77d29ff83bfb934efd6faa2f3
AljonaP <Bartosh_78@hotmail.com>
2022-10-26 16:34:01 +0200

    PatientService:   getPatients(),   getPatientbyId(), updatePatient(), deletePatientById() have been added. transferToDto() have been changes (added Id). PatientController: GetMapping and GetMapping by Id, PutMapping,  DeleteMapping have been added. AppointmentService: in transferToDto() Id has been added.

--- src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/PatientService.java
+++ src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/PatientService.java
@@ -2,9 +2,14 @@ package nl.haaientanden.eindopdrachtbackendtandartspraktijk.services;
 
 import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.PatientDto;
 import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.PatientInputDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.exceptions.RecordNotFoundException;
 import nl.haaientanden.eindopdrachtbackendtandartspraktijk.models.Patient;
 import nl.haaientanden.eindopdrachtbackendtandartspraktijk.repositories.PatientRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static nl.haaientanden.eindopdrachtbackendtandartspraktijk.utils.UtilityMethodes.checkPostcode;
 
@@ -25,6 +30,43 @@ public class PatientService {
         return transferToDto(patient);
     }
 
+    public List<PatientDto> getPatients() {
+
+        List<Patient> patients = patientRepository.findAll();
+        List<PatientDto> dtos = new ArrayList<>();
+        for (Patient patient : patients) {
+            dtos.add(transferToDto(patient));
+        }
+        return dtos;
+    }
+
+    public PatientDto getPatientById(Long id) {
+        if(patientRepository.findById(id).isPresent()) {
+            Patient patient = patientRepository.findById(id).get();
+            return transferToDto(patient);
+        } else {
+            throw new RecordNotFoundException("De ingevoerde value is niet juist. Zoek opnieuw.");
+        }
+    }
+
+    public PatientDto updatePatient(Long id, PatientInputDto inputDto) {
+        if(patientRepository.findById(id).isPresent()) {
+            Patient patient = patientRepository.findById(id).get();
+            Patient patient1 = transferToPatient(inputDto);
+            patient1.setId(patient.getId());
+
+            patientRepository.save(patient1);
+
+            return transferToDto(patient1);
+        } else {
+            throw new RecordNotFoundException("geen patient is gevonden");
+        }
+    }
+
+    public void deletePatientById(@RequestBody Long id) {
+        patientRepository.deleteById(id);
+    }
+
     public static Patient transferToPatient(PatientInputDto dto) {
 
         var patient = new Patient();
@@ -42,11 +84,7 @@ public class PatientService {
 //        if (validPhoneNumber(inputPhoneNumber)) {
 //            patient.setPhoneNumber(dto.getPhoneNumber());
 //        }
-//
-        if (validPhoneNumber(dto.getPhoneNumber())) {
-            patient.setPhoneNumber(dto.getPhoneNumber());
-        }
-//        patient.setPhoneNumber(dto.getPhoneNumber());
+        patient.setPhoneNumber((dto.getPhoneNumber()));
         patient.setReimburseByInsurancePercentage(dto.getReimburseByInsurancePercentage());
 
         return patient;
@@ -58,6 +96,7 @@ public class PatientService {
         String zipCode = patient.getZipCode();
         String inputPhoneNumber = patient.getPhoneNumber();
 
+        dto.setId(patient.getId());
         dto.setNamePatient(patient.getNamePatient());
         dto.setSurnamePatient(patient.getSurnamePatient());
         dto.setDob(patient.getDob());
@@ -69,13 +108,14 @@ public class PatientService {
 //        if (validPhoneNumber(inputPhoneNumber)) {
 //            dto.setPhoneNumber(patient.getPhoneNumber());
 //        }
-        if (validPhoneNumber(patient.getPhoneNumber())) {
-            dto.setPhoneNumber(patient.getPhoneNumber());
-        }
-//        dto.setPhoneNumber(patient.getPhoneNumber());
+        dto.setPhoneNumber(patient.getPhoneNumber());
         dto.setReimburseByInsurancePercentage(patient.getReimburseByInsurancePercentage());
 
         return dto;
     }
+
+//    public static boolean validPhoneNumber(String inputPhoneNumber) {
+//        return inputPhoneNumber.charAt(0) == '0' && inputPhoneNumber.length() == 10 && inputPhoneNumber.matches("^0-9]$");
+//    }
 
 }
