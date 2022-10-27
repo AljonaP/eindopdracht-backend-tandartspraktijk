8934b7e0b1b020f9c4ff194136a19450d7bff9a9
AljonaP <Bartosh_78@hotmail.com>
2022-10-27 13:39:06 +0200

    TreatmentRoomService: transferToTreatmentRoom() and transferToDto() have been added

--- src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/TreatmentRoomService.java
+++ src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/TreatmentRoomService.java
@@ -1,5 +1,8 @@
 package nl.haaientanden.eindopdrachtbackendtandartspraktijk.services;
 
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.TreatmentRoomDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.TreatmentRoomInputDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.models.TreatmentRoom;
 import nl.haaientanden.eindopdrachtbackendtandartspraktijk.repositories.TreatmentRoomRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +12,22 @@ public class TreatmentRoomService {
 
     public TreatmentRoomService(TreatmentRoomRepository treatmentRoomRepository) {
         this.treatmentRoomRepository = treatmentRoomRepository;
+    }
+
+    public static TreatmentRoom transferToTreatmentRoom(TreatmentRoomInputDto dto) {
+        var treatmentRoom = new TreatmentRoom();
+
+        treatmentRoom.setRoomNumber(dto.getRoomNumber());;
+        treatmentRoom.setRoomColor(dto.getRoomColor());
+
+        return treatmentRoom;
+    }
+
+    public static TreatmentRoomDto transferToDto(TreatmentRoom treatmentRoom) {
+        TreatmentRoomDto dto = new TreatmentRoomDto();
+
+        dto.setId(treatmentRoom.getId());
+        dto.setRoomNumber(treatmentRoom.getRoomNumber());
+        dto.setRoomColor(treatmentRoom.getRoomColor());
     }
 }
