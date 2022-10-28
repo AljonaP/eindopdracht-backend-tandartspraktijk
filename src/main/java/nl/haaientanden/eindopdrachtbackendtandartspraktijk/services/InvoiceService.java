6e16aa09cbeb37f3ef533d161ce379b938503ab5
AljonaP <Bartosh_78@hotmail.com>
2022-10-28 12:53:59 +0200

    InvoiceService: fromDtoToInvoice() and fromInvoiceToDto() have been added

--- src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/InvoiceService.java
+++ src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/InvoiceService.java
@@ -1,5 +1,8 @@
 package nl.haaientanden.eindopdrachtbackendtandartspraktijk.services;
 
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.InvoiceDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.dtos.InvoiceInputDto;
+import nl.haaientanden.eindopdrachtbackendtandartspraktijk.models.Invoice;
 import nl.haaientanden.eindopdrachtbackendtandartspraktijk.repositories.InvoiceRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +12,25 @@ public class InvoiceService {
 
     public InvoiceService(InvoiceRepository invoiceRepository) {
         this.invoiceRepository = invoiceRepository;
+    }
+
+    public static Invoice transferToInvoice(InvoiceInputDto dto) {
+
+        var invoice = new Invoice();
+
+        invoice.setInvoiceDate(dto.getInvoiceDate());
+        invoice.setInvoiceNumber(dto.getInvoiceNumber());
+
+        return invoice;
+    }
+
+    public static InvoiceDto transferToDto(Invoice invoice) {
+
+        InvoiceDto dto = new InvoiceDto();
+
+        dto.setInvoiceDate(invoice.getInvoiceDate());
+        dto.setInvoiceNumber(invoice.getInvoiceNumber());
+
+        return dto;
     }
 }
