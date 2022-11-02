d39b3b7517680430cbbdae121c28ddfd790f5a07
AljonaP <Bartosh_78@hotmail.com>
2022-11-02 15:09:07 +0100

    AppointmentTreatment got @ManyToOne relations, constructor, getters & setters

--- src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/models/AppointmentTreatment.java
+++ src/main/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/models/AppointmentTreatment.java
@@ -1,12 +1,44 @@
 package nl.haaientanden.eindopdrachtbackendtandartspraktijk.models;
 
 
-import javax.persistence.Embedded;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class AppointmentTreatment {
     @EmbeddedId
     private AppointmentTreatmentKey id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("appointmentId")
+    @JoinColumn(name = "appointment_id")
+    private Appointment appointment;
+
+    @ManyToOne
+    @MapsId("treatmentId")
+    @JoinColumn(name = "treatment_id")
+    private Treatment treatment;
+
+    public AppointmentTreatmentKey getId() {
+        return id;
+    }
+
+    public void setId(AppointmentTreatmentKey id) {
+        this.id = id;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
+
+    public Treatment getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(Treatment treatment) {
+        this.treatment = treatment;
+    }
 }
