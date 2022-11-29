ee29ac0af78d442718507cc22117a835feaae415
AljonaP <Bartosh_78@hotmail.com>
2022-11-29 11:28:28 +0100

    TreatmentServiceTest has been created.

---
+++ src/test/java/nl/haaientanden/eindopdrachtbackendtandartspraktijk/services/TreatmentServiceTest.java
@@ -0,0 +1,50 @@
+package nl.haaientanden.eindopdrachtbackendtandartspraktijk.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreatmentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void saveTreatment() {
+    }
+
+    @Test
+    void getTreatments() {
+    }
+
+    @Test
+    void getTreatmentById() {
+    }
+
+    @Test
+    void updateTreatment() {
+    }
+
+    @Test
+    void deleteTreatmentById() {
+    }
+
+    @Test
+    void transferToTreatment() {
+    }
+
+    @Test
+    void transferToDto() {
+    }
+
+    @Test
+    void transferTreatmentListToDtoList() {
+    }
+}
